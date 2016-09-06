df7e76b244a6936745e79cc31b3593c0497bef31
Librae <librae8226@gmail.com>
2016-09-07 00:37:06 +0800

    add status indication

--- src/app/Main.js
+++ src/app/Main.js
@@ -7,19 +7,57 @@
 import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
+import RefreshIndicator from 'material-ui/RefreshIndicator';
 import FlatButton from 'material-ui/FlatButton';
+import SVGIconActionPower from 'material-ui/svg-icons/action/power-settings-new'
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import http from 'http';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Paper from 'material-ui/Paper';
+
+import {
+  deepOrange500,
+  grey200,
+  cyan500,
+} from 'material-ui/styles/colors';
 
 const styles = {
   container: {
     textAlign: 'center',
-    paddingTop: '15%',
+    //paddingTop: '15%',
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10,
   },
   button: {
     margin: 6,
+  },
+  refresh: {
+    display: 'inline-block',
+    position: 'relative',
+  },
+  card: {
+    textAlign: 'center',
+    width: '240px',
+    height: '300px',
+  },
+  paper: {
+    width: 240,
+    height: 300,
+    margin: 10,
+    float: 'left',
+  },
+  heading: {
+    fontSize: 20,
+    paddingTop: 19,
+    marginBottom: 13,
+    letterSpacing: 0,
+    backgroundColor: grey200,
+    textAlign: 'center',
+    margin: 0,
+    padding: 0,
+    lineHeight: '50px',
   },
 };
 
@@ -35,9 +73,13 @@ class Main extends Component {
 
     this.handleRequestClose = this.handleRequestClose.bind(this);
     this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.handleTouchTapStart = this.handleTouchTapStart.bind(this);
+    this.handleTouchTapStop = this.handleTouchTapStop.bind(this);
 
     this.state = {
       open: false,
+      status: 'hide',
+      ready: false, // get from real status
     };
   }
 
@@ -48,13 +90,40 @@ class Main extends Component {
   }
 
   handleTouchTapFire() {
-    location.href = '/red';
   }
 
   handleTouchTap() {
     this.setState({
       open: true,
     });
+  }
+
+  doStart() {
+    this.setState({status: 'loading'});
+    setTimeout(this.onDoStarted.bind(this), 2000);
+  }
+
+  onDoStarted() {
+    this.setState({status: 'hide', ready: true});
+  }
+
+  doStop() {
+    this.setState({status: 'loading'});
+    setTimeout(this.onDoStopped.bind(this), 1000);
+  }
+
+  onDoStopped() {
+    this.setState({status: 'hide', ready: false});
+  }
+
+  handleTouchTapStop() {
+    this.doStop();
+    console.log(this.state);
+  }
+
+  handleTouchTapStart() {
+    this.doStart();
+    console.log(this.state);
   }
 
   render() {
@@ -76,13 +145,32 @@ class Main extends Component {
           >
             Are you sure ready to take the adventure?
           </Dialog>
-          <h1>Material-UI</h1>
-          <h2>the cloud of REDs</h2>
-          <RaisedButton
-            label="Super Secret World"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
+          <Paper style={styles.paper}>
+            <h1 style={styles.heading}>Material-UI</h1>
+            <h2>the cloud of REDs</h2>
+            <RaisedButton
+              label="Super Secret World"
+              secondary={true}
+              onTouchTap={this.handleTouchTap}
+            />
+          </Paper>
+          <Paper style={styles.paper}>
+            <h1 style={styles.heading}>Power</h1>
+            <FlatButton
+              label={this.state.ready ? "Stop" : "Start"}
+              primary={!this.state.ready}
+              secondary={this.state.ready}
+              onTouchTap={this.state.ready ? this.handleTouchTapStop : this.handleTouchTapStart}
+              icon={<SVGIconActionPower />}
+            />
+            <RefreshIndicator
+              size={30}
+              left={10}
+              top={8}
+              status={this.state.status}
+              style={styles.refresh}
+            />
+          </Paper>
         </div>
       </MuiThemeProvider>
     );
